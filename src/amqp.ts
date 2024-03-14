175025f2593965b34941352f260747e4be0b3fe7
My CTO <mycto@proton.me>
2024-03-14 19:58:39 +0100

    Optionally register json message schema for publishing to amqp topics

--- src/amqp.ts
+++ src/amqp.ts
@@ -3,7 +3,26 @@ const rabbi = require('rabbi');
 
 const exchange = process.env.PRICES_AMQP_EXCHANGE || 'prices';
 
+import { Schema } from '@hapi/joi';
+
+const schemas: { [key: string]: Schema } = {}
+
+export function registerSchema(name: string, schema: Schema) {
+    schemas[name] = schema;
+}
+
 export async function publish(topic: string, payload: any) {
+
+    const schema = schemas[topic];
+
+    if (schema) {
+
+        const validation = schema.validate(payload);
+        if (validation.error) {
+            throw new Error(`Invalid payload for topic ${topic}: ${validation.error.message}`);
+        }
+    }
+
 
     return rabbi.publish(exchange, topic, {
         topic, payload
--- src/server/websockets/subscriptions.ts
+++ src/server/websockets/subscriptions.ts
@@ -1,3 +1,5 @@
+import Joi from "@hapi/joi"
+import { registerSchema } from "../../amqp"
 
 export const topics = [
     'price/updated',
@@ -8,3 +10,21 @@ export const topics = [
     'sourceRemoved',
     'sourceRemoved'
 ]
+
+registerSchema(
+    'price/updated',
+    Joi.object({
+        quote: Joi.string().required(),
+        base: Joi.string().required(),
+        value: Joi.number().required(),
+        source: Joi.string().required(),
+        updated_at: Joi.date().required()
+    }).label('PriceUpdated')
+)
+
+registerSchema(
+    'app.started',
+    Joi.object({
+        timestamp: Joi.date().required()
+    }).label('AppStarted')
+)
